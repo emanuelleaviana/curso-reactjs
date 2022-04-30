0c95034bf8099eac2c745422aa9af916bf7c9aa9
Emanuelle Viana <manuavbr@gmail.com>
2022-04-30 12:43:10 -0300

    update firebase

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,5 +1,17 @@
+import {useState} from "react";
+
 export function Button() {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+    
+    
     return (
-        <button> </button>
+        <button onClick={increment}>
+            {counter}
+        </button>
     )
 }
